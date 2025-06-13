8ca7042173d0483597f0260632caf264f2af8dac
shakurShirajul <siirajulisllam@gmail.com>
2025-06-13 18:38:16 +0600

    MONGODB_URI

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-// const MONGODB_URI = `mongodb://localhost:27017/nextjs-blog`;
+// const MONGODB_URI = `mongodb://localhost:27017`;
 const MONGODB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mydatabase.1c7whlf.mongodb.net/?retryWrites=true&w=majority&appName=myDatabase`;
 
 if (!MONGODB_URI) {
@@ -17,9 +17,11 @@ async function connectDB() {
     return cached.conn;
   }
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
-      return mongoose;
-    });
+    cached.promise = mongoose
+      .connect(MONGODB_URI, { dbName: "nextjs-blog" })
+      .then((mongoose) => {
+        return mongoose;
+      });
   }
 
   cached.conn = await cached.promise;
